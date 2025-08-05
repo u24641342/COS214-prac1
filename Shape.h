92f5635262c74ddc6082198ffd808f5e0adfbfe7
Mulondi Makhado <makhadomulondi@gmail.com>
2025-08-05 12:59:55 +0200

    Implemented the Shape.h

--- Shape.h
+++ Shape.h
@@ -1,4 +1,16 @@
 #ifndef SHAPE_H
 #define SHAPE_H
+#include <string>
+using namespace std;
+class Shape {
+    private:
+        int length;
+        int width;
+        string colour;
+        int position_X;
+        int position_Y;
+    public: 
+        virtual Shape* clone() = 0;
+};
 
 #endif
