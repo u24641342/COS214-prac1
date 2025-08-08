ba6e6b92ae732fb4d47e5f7b0448d56c818e418a
Mulondi Makhado <makhadomulondi@gmail.com>
2025-08-09 00:12:04 +0200

    Refactor ShapeFactory methods to include const qualifier for toString() and improve code consistency

--- ShapeFactory.cpp
+++ ShapeFactory.cpp
@@ -3,18 +3,21 @@
 Shape* SquareFactory::createShape(int length = 0, int width = 0, string colour = "white", int posX = 0, int posY = 0, string text = "") const {
     return new Square(length, colour, posX, posY);
 }
-void SquareFactory::toString() {
+void SquareFactory::toString() const {
     // Implementation for Square
+    cout << "Square Factory: Creates a Square shape." << endl;
 }
 Shape* RectangleFactory::createShape(int length = 0, int width = 0, string colour = "white", int posX = 0, int posY = 0, string text = "") const {
     return new Rectangle(length, width, colour, posX, posY);
 }
-void RectangleFactory::toString() {
+void RectangleFactory::toString() const {
     // Implementation for Rectangle
+    cout << "Rectangle Factory: Creates a Rectangle shape." << endl;
 }
 Shape* TextboxFactory::createShape(int length = 0, int width = 0, string colour = "white", int posX = 0, int posY = 0, string text = "") const {
     return new Textbox(length, width, colour, posX, posY, text);
 }
-void TextboxFactory::toString() {
+void TextboxFactory::toString() const {
     // Implementation for Textbox
+    cout << "Textbox Factory: Creates a Textbox shape." << endl;
 }
