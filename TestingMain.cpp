a7396ad7504e46dbd6ff340e354aa3f9f8896e52
u24641342 <u24641342@tuks.co.za>
2025-08-10 13:41:46 +0200

    testing template mthod

--- TestingMain.cpp
+++ TestingMain.cpp
@@ -9,42 +9,44 @@ int main()
 {
     // Create a canvas
     Canvas canvas;
-    // Create shape factories
-    SquareFactory squareFactory;
-    RectangleFactory rectangleFactory;
-    TextboxFactory textboxFactory;
-    // Create shapes using factories
-    Shape* square1 = squareFactory.createShape(5, 0, "blue", 10, 10);
-    Shape* rectangle1 = rectangleFactory.createShape(10, 5, "red", 20, 20);
-    Shape* textbox1 = textboxFactory.createShape(15, 10, "green", 30, 30, "Hello World");
-    Shape* square2 = square1->clone();
-    Shape* rectangle2 = rectangle1->clone();
-    Shape* textbox2 = textbox1->clone();
-    canvas.addShape(square1);
-    canvas.addShape(rectangle1);
-    canvas.addShape(textbox1);
-    canvas.addShape(square2);
-    canvas.addShape(rectangle2);
-    canvas.addShape(textbox2);
-    // Capture the current state of the canvas
-    Memento* memento = canvas.captureCurrent();
+    // // Create shape factories
+    // SquareFactory squareFactory;
+    // RectangleFactory rectangleFactory;
+    // TextboxFactory textboxFactory;
+    // // Create shapes using factories
+    // Shape* square1 = squareFactory.createShape(5, 0, "blue", 10, 10);
+    // Shape* rectangle1 = rectangleFactory.createShape(10, 5, "red", 20, 20);
+    // Shape* textbox1 = textboxFactory.createShape(15, 10, "green", 30, 30, "Hello World");
+    // Shape* square2 = square1->clone();
+    // Shape* rectangle2 = rectangle1->clone();
+    // Shape* textbox2 = textbox1->clone();
+    // canvas.addShape(square1);
+    // canvas.addShape(rectangle1);
+    // canvas.addShape(textbox1);
+    // canvas.addShape(square2);
+    // canvas.addShape(rectangle2);
+    // canvas.addShape(textbox2);
+    // // Capture the current state of the canvas
+    // Memento* memento = canvas.captureCurrent();
     // Export the canvas to a file
-    PNGExporter pngExporter;
+    PNGExporter pngExporter(&canvas);
+    PDFExporter pdfExporter(&canvas);
     pngExporter.exportToFile();
-    // Clear the canvas
-    canvas.clearCanvas();
-    // Undo the last action
-    canvas.undoAction(memento);
-    // Clean up
-    delete square1;
-    delete rectangle1;   
-    delete textbox1;
-    delete square2;
-    delete rectangle2;   
-    delete textbox2;
-    memento->deleteMemento();
-    delete memento;
-    std::cout << "Testing completed successfully." << std::endl;
+    pdfExporter.exportToFile();
+    // // Clear the canvas
+    // canvas.clearCanvas();
+    // // Undo the last action
+    // canvas.undoAction(memento);
+    // // Clean up
+    // delete square1;
+    // delete rectangle1;   
+    // delete textbox1;
+    // delete square2;
+    // delete rectangle2;   
+    // delete textbox2;
+    // memento->deleteMemento();
+    // delete memento;
+    // std::cout << "Testing completed successfully." << std::endl;
 
     return 0;
 }
--- makefile
+++ makefile
@@ -1,18 +1,18 @@
 
-filesToDelete = main list task todo weekly priority history manager
-filesToCompile = main.cpp list.cpp task.cpp todo.cpp weekly.cpp priority.cpp history.cpp manager.cpp 
-filesInCoverageReport = main list task todo weekly priority history manager 
+filesToDelete = TestingMain Canvas ExportCanvas
+filesToCompile = TestingMain.cpp Canvas.cpp ExportCanvas.cpp
+filesInCoverageReport = TestingMain Canvas ExportCanvas
 
 main:
-	g++ -g -std=c++98 ${filesToCompile} -o main
+	g++ -g -std=c++11 ${filesToCompile} -o TestingMain
 
 clean:
 	rm *.o *.gcov *.gcda *.gcno *.gz *.html *.css output.txt coverage.txt -f ${filesToDelete}
 
 coverage:
 	make clean
-	g++ -g --coverage -dumpbase '' ${filesToCompile} -o main
-	./main > outputXD.txt
+	g++ -g --coverage -dumpbase '' ${filesToCompile} -o TestingMain
+	./TestingMain > outputXD.txt
 	gcov -f -m -r -j ${filesInCoverageReport} > coverage.txt 
 	gcovr --html-details output.html
 	#make clean
