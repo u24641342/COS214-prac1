3abba3f315f211cbbbbdd778540536f752d68941
Mulondi Makhado <makhadomulondi@gmail.com>
2025-08-10 21:31:53 +0200

    Uncomment shape cloning and canvas state capture in TestingMain.cpp

--- TestingMain.cpp
+++ TestingMain.cpp
@@ -17,17 +17,17 @@ int main()
     Shape* square1 = squareFactory.createShape(5, 0, "blue", 10, 10);
     Shape* rectangle1 = rectangleFactory.createShape(10, 5, "red", 20, 20);
     Shape* textbox1 = textboxFactory.createShape(15, 10, "green", 30, 30, "Hello World");
-    // Shape* square2 = square1->clone();
-    // Shape* rectangle2 = rectangle1->clone();
-    // Shape* textbox2 = textbox1->clone();
+    Shape* square2 = square1->clone();
+    Shape* rectangle2 = rectangle1->clone();
+    Shape* textbox2 = textbox1->clone();
     canvas.addShape(square1);
     canvas.addShape(rectangle1);
     canvas.addShape(textbox1);
-    // canvas.addShape(square2);
-    // canvas.addShape(rectangle2);
-    // canvas.addShape(textbox2);
-    // // Capture the current state of the canvas
-    // Memento* memento = canvas.captureCurrent();
+    canvas.addShape(square2);
+    canvas.addShape(rectangle2);
+    canvas.addShape(textbox2);
+    // Capture the current state of the canvas
+    Memento* memento = canvas.captureCurrent();
     // Export the canvas to a file
     PNGExporter pngExporter(&canvas);
     PDFExporter pdfExporter(&canvas);
